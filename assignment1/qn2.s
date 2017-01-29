c251326d323165c024a1ef23220262da957913fd
kala sagar pogiri <pogirikalasagar@gmail.com>
2017-01-29 21:40:06 +0530

    With string scaning

--- assignment1/qn2.s
+++ assignment1/qn2.s
@@ -1,59 +1,55 @@
 #Data Declarations
 
 .data 
-
-string: .asciiz "bcd%BADShA_+#"
-length: .word 13
+msg1: .asciiz "Please insert text (max 20 characters) : "
+msg2: .asciiz "\nFinal String is : "
+string: .space 22
+#length: .word 0
 
 .text
 
-main:
+main: 
+    la $a0, msg1     
+    li $v0, 4
+    syscall
 
-    # s0 stores starting adress of string
+    la $a0, string      # address of input
+    li $a1, 22       # initialize a1 to max-length of string
+
+    li $v0, 8           # read input string
+    syscall
+
     la $s0, string
-    
-    # initialize count
-    li $a1, 1
-    li $a2, 13
+    lb $t0, ($s0)
+    beq $t0, $zero, end
+    blt $t0, 'a', loop
+    bgt $t0, 'z', loop
 
+    #chage aplhabet
+    sub $t0, $t0, 32
+    sb $t0, ($s0)
 loop:
-    
-    bgt $a1, $a2, end
-    
+    addi $s0, $s0, 1
     lb $t0, ($s0)
 
-    blt $t0, 'a', loop1
-    bgt $t0, 'z', loop1
+    beq $t0, $zero, end
+    blt $t0, 'a', loop
+    bgt $t0, 'z', loop
 
     #chage aplhabet
     sub $t0, $t0, 32
     sb $t0, ($s0)
 
-    #move $a0, $t0
-    #li $v0, 11
-    #syscall
-    
-    addi $s0, $s0, 1
-    addi $a1, $a1, 1
-    j loop
-
-loop1:
-    #move $a0, $t0
-    #li $v0, 11
-    #syscall
-    
-    # FORWARD THE ADDRESS
-    addi $s0, $s0, 1
-    addi $a1, $a1, 1
     j loop
 
 end:
-
-    la $a0, string
-
+    la $a0, msg2
     li $v0, 4
     syscall
 
+    la $a0, string
+    li $v0, 4
+    syscall
 
     li $v0, 10
     syscall
