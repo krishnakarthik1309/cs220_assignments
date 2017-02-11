ed083d70e35a3b9ffe4bfb7f1ed08524a1154fa8
krishnakarthik1309 <krishnakarthik1309@gmail.com>
2017-02-11 16:17:22 +0530

    assgn6 complete

--- assignment5_6/assgn6.s
+++ assignment5_6/assgn6.s
@@ -29,16 +29,9 @@ main:
 	add $t4, $v0, $zero
 	move $t5, $zero	# count
 
-	# li $v0, 1
-	# syscall
-
-	# move $a0, $t3
-	# li $v0, 1
-	# syscall
-
 	# Read n integers
 	read:
-		bge $t5, $t6, bubble_sort
+		bge $t5, $t6, prepare_bubble_sort
 
 		# print prompt
 		li $v0, 4
@@ -54,15 +47,38 @@ main:
 		addi $t5, $t5, 1
 		b read
 
-	bubble_sort:
-		b prepare_print
+	prepare_bubble_sort:
+		addi $t7, $t6, -1	# N - 1
+		add $t3, $t4, $zero
+		move $t5, $zero		# outer_loop count 'i'
 
-	# swap contents in $t0, $t1 using $t2 as temp register
-	swap:
-		move $t2, $t0
-		move $t0, $t1
-		move $t1, $t2
-		jr $ra
+	outer_loop:
+		bge $t5, $t7, prepare_print
+
+		sub $t9, $t7, $t5	# store N - 1 - i in $t9
+		move $t8, $zero 	# inner_loop count 'j'
+
+		inner_loop:
+			bge $t8, $t9, exit_inner_loop
+
+			lw $t0, ($t3)
+			lw $t1, 4($t3)
+			
+			ble $t0, $t1, skip_swap
+			jal swap
+			
+			skip_swap:
+
+			# prepare for next inner_loop iteration
+			addi $t8, $t8, 1
+			addi $t3, $t3, 4
+			b inner_loop
+
+		# prepare for next outer_loop iteration
+		exit_inner_loop:
+			addi $t5, $t5, 1
+			add $t3, $t4, $zero
+			b outer_loop
 
 	# print array in sorted order
 	prepare_print:
@@ -72,26 +88,37 @@ main:
 
 		move $t5, $zero	# count
 
-		print:
-			bge $t5, $t6, terminate
+	print:
+		bge $t5, $t6, terminate
 
-			li $v0, 4
-			la $a0, space
-			syscall
-			
-			# print number
-			lw $a0, ($t4)
-			li $v0, 1
-			syscall
+		li $v0, 4
+		la $a0, space
+		syscall
+		
+		# print number
+		lw $a0, ($t4)
+		li $v0, 1
+		syscall
 
 
-			# prepare for next iteration
-			addi $t4, $t4, 4
-			addi $t5, $t5, 1
-			b print
+		# prepare for next iteration
+		addi $t4, $t4, 4
+		addi $t5, $t5, 1
+		b print
 
 	terminate:
 		li $v0, 10
 		syscall
 
 .end main
+
+# swap contents in $t0, $t1 using $t2 as temp register
+swap:
+	move $t2, $t0
+	move $t0, $t1
+	move $t1, $t2
+
+	sw $t0, ($t3)
+	sw $t1, 4($t3)
+
+	jr $ra
